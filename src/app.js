e03a73f0213050ab14ba2036781ec6c111a8c78a
Cadivals <4geeksalex@gmail.com>
2024-10-20 15:11:24 +0000

    feast:domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  window.onload = function() {
+    let pronoun = ["the", "our", "his"];
+    let adj = ["great", "big", "small", "kind"];
+    let noun = ["jogger", "racoon", "tiger", "man"];
+    let dom = [".com", ".org", ".es", ".io"];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            console.log(pronoun[i] + adj[j] + noun[k] + dom[l]);
+          }
+        }
+      }
+    }
+  };
 };
